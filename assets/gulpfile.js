ffdaf45c9e65f16050c1446069b41ba6135a37d0
Matthew Schroeter <schroeter.m83@gmail.com>
2021-04-06 17:52:37 -0500

    updated gulp to run jekyll command when html file changes

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,11 +1,12 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
-var fs            = require('fs')
-var browsers      = JSON.parse(fs.readFileSync('./.browserslistrc'))
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync').create();
+const $             = require('gulp-load-plugins')();
+const autoprefixer  = require('autoprefixer');
+const fs            = require('fs')
+const browsers      = JSON.parse(fs.readFileSync('./.browserslistrc'))
+const gutil         = require('gulp-util');
 
-var sassPaths = [
+const sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
@@ -24,13 +25,22 @@ function sass() {
     .pipe(browserSync.stream());
 }
 
+// Run jekyll build command.
+function jekyll() {
+  const shellCommand = 'cd .. && bundle exec jekyll build --disable-disk-cache --config _config.yml';
+  return gulp.src('/', {allowEmpty: true})
+      .pipe($.run(shellCommand))
+      .on('error', gutil.log)
+      .pipe(gulp.dest('assets'))
+}
+
 function serve() {
   browserSync.init({
     server: "../_site/"
   });
 
-  gulp.watch("scss/**/*.scss", sass);
-  gulp.watch("../**/*.html").on('change', browserSync.reload);
+  gulp.watch('scss/**/*.scss', gulp.series([sass, jekyll]));
+  gulp.watch(['../**/*.html', '!../_site/**/*.html'], gulp.series(jekyll)).on('change', browserSync.reload);
 }
 
 gulp.task('sass', sass);
